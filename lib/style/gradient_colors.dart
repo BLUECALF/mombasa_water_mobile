e4ebba8956c254703cae6f6ea56c36062c2d636e
BLUECALF <leonardkiptala@gmail.com>
2022-07-20 18:19:14 +0300

    ADD:Added custom icons to the buttons

---
+++ lib/style/gradient_colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+/// WE NEED GRADIENTS FOR COLORS WELL USE IN THE APP
+Gradient g1 = LinearGradient(
+  colors: [
+    Colors.blue,
+    Colors.lightBlueAccent
+  ],
+);
+Gradient g2 = LinearGradient(colors: [
+  Colors.yellowAccent,
+  Colors.white
+]);
+Gradient g3 = LinearGradient(colors: [
+  Colors.green,
+  Colors.lightGreenAccent
+]);
+Gradient g4 = LinearGradient(colors: [
+  Colors.yellowAccent,
+  Colors.lightGreenAccent
+]);
+Gradient g5 = LinearGradient(
+  colors: [
+    // Colors.blue,
+    Colors.lightGreenAccent,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.lightGreenAccent,
+  ],
+);
+Gradient g6 = LinearGradient(colors: [
+  Colors.purple,
+  Colors.purpleAccent
+]);
