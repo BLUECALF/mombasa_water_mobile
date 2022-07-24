0df4ab4eb3d1e90619ea66175b19d4df2b7cacea
BLUECALF <leonardkiptala@gmail.com>
2022-07-25 00:38:49 +0300

    Fix: made homepage_personal look good

--- lib/pages/home/homepage_personal.dart
+++ lib/pages/home/homepage_personal.dart
@@ -26,86 +26,84 @@ class HomePagePersonal extends GetView<HomeController>{
       endDrawer: homeController.makeEndDrawer(),
       appBar: homeController.make_appBar(),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text("Mombasa Water",
-                style:TextStyle(
-                    fontSize: 18,
-                    color: Colors.lightBlueAccent,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 3
-                )
-            ),
-            SizedBox(height: 20),
-            Text("Welcome Back ${appController.current_user.value}",style:TextStyle(
-                fontSize: 20,
-                color: Colors.black54,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 3
-            )),
+        child: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Welcome Back ${appController.current_user.value},",style:TextStyle(
+                  fontSize: 20,
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3
+              )),
 
-            SizedBox(height: 20),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Container(
-                height: 150,width: 300,
-                child: GradientCard(
-                  gradient:g5,
-                  child: Column(
-                    children: [
-                      SizedBox(height: 20,),
-                      Text("Your Balance",
-                          style:TextStyle(
-                              fontSize: 18,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 3
-                          )
-                      ),
-                      SizedBox(height: 20,),
-                      Text("Ksh .",
-                          style:TextStyle(
-                              fontSize: 18,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 3
-                          )
-                      ),
-                      SizedBox(height: 20,),
-                      Text("Last Payment is Ksh.",
-                          style:TextStyle(
-                              fontSize: 14,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w100,
-                              letterSpacing: 3
-                          )
-                      ),
-                    ],
-                  )),
+              SizedBox(height: 20),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Container(
+                  height: 170,width: Get.width,
+                  child: Center(
+                    child: GradientCard(
+                      gradient:g5,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 20,),
+                          SizedBox(width: Get.width,),
+                          Text("Your Balance",
+                              style:TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 3
+                              )
+                          ),
+                          SizedBox(height: 20,),
+                          Text("Ksh .",
+                              style:TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 3
+                              )
+                          ),
+                          SizedBox(height: 20,),
+                          Text("Last Payment is Ksh.",
+                              style:TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  letterSpacing: 2
+                              )
+                          ),
+                          SizedBox(height: 20,),
+                        ],
+                      )),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  homeController. make_button_with_png(path: "others/svgtopng/pay-icon.png",function: (){Get.to(PayPage());},text: "Pay"),
-                  homeController.make_button(icon_name: MwIcons.query_bill_icon,function: (){Get.to(QueryBillPage());}, text: "Query Bill"),
-                  homeController.make_button(icon_name: MwIcons.illegal_fee,function: (){Get.to(IllegalFeePage());}, text: "Illegal Fee"),
-                  homeController.make_button_with_png(path: "others/svgtopng/other-payments.png",function: (){Get.to(OtherPaymentsPage());},text: "Other\nPayments"),
-                ],
+              SizedBox(height: 20),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    homeController. make_button_with_png(path: "others/svgtopng/pay-icon.png",function: (){Get.to(PayPage());},text: "Pay"),
+                    homeController.make_button(icon_name: MwIcons.query_bill_icon,function: (){Get.to(QueryBillPage());}, text: "Query Bill"),
+                    homeController.make_button(icon_name: MwIcons.illegal_fee,function: (){Get.to(IllegalFeePage());}, text: "Illegal Fee"),
+                    homeController.make_button_with_png(path: "others/svgtopng/other-payments.png",function: (){Get.to(OtherPaymentsPage());},text: "Other\nPayments"),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            Text("Transactions",style:TextStyle(
-                fontSize: 20,
-                color: Colors.black54,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 3
-            )),
-          ],
+              SizedBox(height: 30),
+              Text("Transactions", style:TextStyle(
+                  fontSize: 19,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2
+              )),
+            ],
+          ),
         ),
       ),
     );
