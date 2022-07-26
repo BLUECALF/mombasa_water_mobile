20e9f36d6fea283251f3a099cfcef65be7aa31ab
BLUECALF <leonardkiptala@gmail.com>
2022-07-26 10:49:13 +0300

    ADD:  Added font to accounts page

--- lib/pages/accounts/accounts_page.dart
+++ lib/pages/accounts/accounts_page.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mombasa_water/AppController/app_controller.dart';
 import 'package:mombasa_water/mw_icons_icons.dart';
+import 'package:mombasa_water/pages/common_page_controller.dart';
 
 class AccountsPage extends GetView {
   AppController appController = Get.find<AppController>();
+  CommonPageController commonPageController = Get.find<CommonPageController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Profile",
             style:TextStyle(
+                fontFamily: "Poppins",
             fontSize: 22,
             color: Colors.black,
             fontWeight: FontWeight.bold,
@@ -30,6 +33,7 @@ class AccountsPage extends GetView {
                 CircleAvatar(child: Image.asset("others/svgtopng/user 1.png",color: Colors.white,),radius: 60,),
                   SizedBox(height: 10),
                 Text("${appController.current_user.value}",style:TextStyle(
+                    fontFamily: "Poppins",
                     fontSize: 22,
                     color: Colors.black,
                     fontWeight: FontWeight.bold,
@@ -38,18 +42,19 @@ class AccountsPage extends GetView {
                   SizedBox(height: 10),
                 Text("Account Number",
                     style:TextStyle(
-                    fontSize: 18,
+                        fontFamily: "Poppins",
+                    fontSize: 14,
                     color: Colors.black26,
-                    fontWeight: FontWeight.bold,
                     letterSpacing: 2
                 )),
                   SizedBox(height: 15),
                 Text("${appController.current_user_data[0]}",
                     style:TextStyle(
-                        fontSize: 16,
+                        fontFamily: "Poppins",
+                        fontSize: 14,
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
-                        letterSpacing: 2
+                        letterSpacing: 3
                     )),
                   SizedBox(height: 20),
                   ListView(
@@ -59,8 +64,12 @@ class AccountsPage extends GetView {
                       make_list_tile(text: "Tell Your Friend", icon_data: Icons.share),
                       make_list_tile(text: "Delete Account", icon_data: MwIcons.trash_icon),
                       Divider(color: Colors.blue,),
-                      TextButton(child: make_list_tile(text: "Logout", icon_data: Icons.power_settings_new,),
-                        onPressed: (){appController.logout(); Get.back();},),
+                      TextButton(child: make_logout_tile(),
+                      onPressed: (){
+                        appController.logout();
+                        commonPageController.selected_index.value = 0;
+                        },),
+
                     ],
                   ),
                 ],
@@ -73,13 +82,41 @@ class AccountsPage extends GetView {
   Widget make_list_tile({required String text,required IconData icon_data})
   {
     return ListTile(
-        title: Text(text),leading: Container(
+        title: Text(text,
+        style:TextStyle(
+          fontFamily: "Poppins",
+        )
+        ),leading: Container(
         padding: EdgeInsets.all(15),
         decoration: new BoxDecoration(
             shape: BoxShape.circle,
             color: Colors.blue
         ),
         child: Icon(icon_data,color: Colors.white))
+    );
+  }
+  Widget make_logout_tile()
+  {
+    return  ListTile(
+        title: Text("Logout",
+            style:TextStyle(
+              color: Colors.blue,
+              fontFamily: "Poppins",
+            )
+        ),leading: Container(
+      padding: EdgeInsets.all(2),
+      decoration: new BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.blue
+      ),
+      child: Container(
+          padding: EdgeInsets.all(15),
+          decoration: new BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white
+          ),
+          child: Icon(Icons.power_settings_new,color: Colors.blue)),
+    )
     );
   }
 }
